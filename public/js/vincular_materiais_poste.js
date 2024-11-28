aed7d41d756adb09be266f541730b47de06ede80
Luiz Gustavo Capatto <gustavocapatto@gmail.com>
2024-11-28 12:20:09 -0300

    Update vincular_materiais_poste.js

--- public/js/vincular_materiais_poste.js
+++ public/js/vincular_materiais_poste.js
@@ -8,13 +8,13 @@ function vincularMateriaisPoste(posteId, posteNome) {
     }
 
     // Criação do modal dinamicamente
-modal = document.createElement('div');
-modal.classList.add('modal', 'fade');
-modal.id = `vincularMateriaisModal_${posteId}`;
-modal.tabIndex = -1;
-modal.setAttribute('aria-labelledby', `vincularMateriaisPosteModalLabel_${posteId}`);
+    modal = document.createElement('div');
+    modal.classList.add('modal', 'fade');
+    modal.id = `vincularMateriaisModal_${posteId}`;
+    modal.tabIndex = -1;
+    modal.setAttribute('aria-labelledby', `vincularMateriaisPosteModalLabel_${posteId}`);
 
-modal.innerHTML = `
+    modal.innerHTML = `
 <div class="modal-dialog modal-dialog-scrollable w-100 mw-100 mx-auto p-4">
     <div class="modal-content shadow-lg rounded-3">
         <div class="modal-header border-0">
@@ -59,27 +59,29 @@ modal.innerHTML = `
         }
     }
 
-// Função que cria a tabela para um nível específico
-function criarTabelaNivel(posteId, nivel = {}) {
-    const newTable = document.createElement('div');
-    newTable.classList.add('mb-4');
-    newTable.innerHTML = `
-    <div class="card border-0 shadow-lg rounded-lg" data-nivel-id="${nivel.id || ''}">
+    // Função que cria a tabela para um nível específico
+    function criarTabelaNivel(posteId, nivel = {}) {
+        const newTable = document.createElement('div');
+        newTable.classList.add('mb-4');
+        newTable.innerHTML = `
+    <div class="card border-0 shadow-sm rounded-lg bg-light" data-nivel-id="${nivel.id || ''}">
         <div class="card-body p-4">
-            <h5 class="card-title text-muted mb-3">Nível</h5>
+            <h5 class="card-title text-primary fw-bold mb-4">Informações do Nível</h5>
             <div class="table-responsive">
-                <table class="table table-borderless table-sm mb-4">
+                <table class="table table-borderless table-sm">
                     <thead>
                         <tr>
-                            <th scope="col" class="col-6">Nome</th>
+                            <th scope="col" class="col-8">Nome</th>
                             <th scope="col" class="col-4 text-center">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td><input type="text" class="form-control form-control-sm" value="${nivel.nome || ''}" placeholder="Nome do Nível" /></td>
+                            <td>
+                                <input type="text" class="form-control form-control-sm" value="${nivel.nome || ''}" placeholder="Nome do Nível" />
+                            </td>
                             <td class="text-center">
-                                <button type="button" class="btn btn-danger btn-sm rounded-circle removeBtn">
+                                <button type="button" class="btn btn-outline-danger btn-sm rounded-circle removeNivelBtn">
                                     <i class="bi bi-trash"></i>
                                 </button>
                             </td>
@@ -88,9 +90,9 @@ function criarTabelaNivel(posteId, nivel = {}) {
                 </table>
             </div>
 
-            <h6 class="text-muted">Materiais</h6>
+            <h6 class="text-muted mb-3">Materiais</h6>
             <div class="table-responsive">
-                <table class="table table-bordered table-sm mb-3">
+                <table class="table table-bordered table-sm">
                     <thead>
                         <tr class="table-light">
                             <th scope="col">Código</th>
@@ -100,18 +102,24 @@ function criarTabelaNivel(posteId, nivel = {}) {
                         </tr>
                     </thead>
                     <tbody>
-                        <!-- Linha inicial de material -->
                         ${nivel.materiais ? nivel.materiais.map(material => `
-                        <tr class="table-row">
-                            <td><input type="text" class="form-control form-control-sm materialCode" value="${material.codigo || ''}" placeholder="Código" /></td>
-                            <td><input type="text" class="form-control form-control-sm materialName" value="${material.nome || ''}" placeholder="Nome" readonly /></td>
-                            <td><input type="number" class="form-control form-control-sm" value="${material.quantidade || ''}" placeholder="Quantidade" /></td>
-                            <td class="text-center">
-                                <button type="button" class="btn btn-danger btn-sm rounded-circle removeMaterialBtn">
-                                    <i class="bi bi-trash"></i>
-                                </button>
-                            </td>
-                        </tr>
+                       <tr>
+    <td>
+        <input type="text" class="form-control form-control-sm materialCode" value="${material.codigo || ''}" placeholder="Código" />
+    </td>
+    <td>
+        <input type="text" class="form-control form-control-sm materialName" value="${material.nome || ''}" placeholder="Nome" readonly />
+    </td>
+    <td>
+        <input type="number" class="form-control form-control-sm" value="${material.quantidade || ''}" placeholder="Quantidade" />
+    </td>
+    <td class="text-center">
+        <button type="button" class="btn btn-outline-danger btn-sm rounded-circle removeMaterialBtn">
+            <i class="bi bi-trash"></i>
+        </button>
+    </td>
+</tr>
+
                         `).join('') : ''}
                     </tbody>
                 </table>
@@ -123,77 +131,110 @@ function criarTabelaNivel(posteId, nivel = {}) {
     </div>
 `;
 
-    // Lógica para remover o nível
-    const removeBtn = newTable.querySelector('.removeBtn');
-    removeBtn.addEventListener('click', () => newTable.remove());
 
-    // Lógica para adicionar materiais
-    const addMaterialBtn = newTable.querySelector('.addMaterialBtn');
-    addMaterialBtn.addEventListener('click', () => adicionarMaterial(newTable));
 
-    // Lógica para adicionar evento 'input' ao campo de código de material
-    const materialCodeInputs = newTable.querySelectorAll('.materialCode');
-    materialCodeInputs.forEach(input => {
-        input.addEventListener('input', () => {
-            const codigo = input.value.trim();
-            // Busca o material na lista com base no código
-            const material = listaDeMateriais.find(item => item.id.toString() === codigo);
+        // Evento para remover a tabela de nível
+        const removeNivelBtn = newTable.querySelector('.removeNivelBtn');
+        removeNivelBtn.addEventListener('click', () => {
+            newTable.remove();
+        });
 
-            const materialNameInput = input.closest('tr').querySelector('.materialName');
+        const removeMaterialBtns = newTable.querySelectorAll('.removeMaterialBtn');
+        removeMaterialBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                btn.closest('tr').remove();
+            });
+        });
+
+
+        // Lógica para adicionar materiais
+        const addMaterialBtn = newTable.querySelector('.addMaterialBtn');
+        addMaterialBtn.addEventListener('click', () => adicionarMaterial(newTable));
+
+        const materialCodeInputs = newTable.querySelectorAll('.materialCode');
+        materialCodeInputs.forEach(input => {
+            input.addEventListener('input', () => {
+                const searchTerm = input.value.trim().toLowerCase();  // Obtém o valor de busca (código ou nome)
+
+
+
+                const materialNameInput = input.closest('tr').querySelector('.materialName');
+                const material = listaDeMateriais.find(item => item.id.toString() === searchTerm || item.nome.toLowerCase().includes(searchTerm));
+
+                if (material) {
+                    materialNameInput.value = material.nome;
+                    input.classList.remove('is-invalid');
+                    materialNameInput.classList.remove('is-invalid');
+                } else {
+                    materialNameInput.value = ''; // Limpa o nome se o material não for encontrado
+                    input.classList.add('is-invalid');
+                    materialNameInput.classList.add('is-invalid');
+                }
+            });
+        });
+
+
+
+
+        return newTable;
+    }
+
+
+
+    /// Função para adicionar material a um nível
+    function adicionarMaterial(newTable) {
+        const materialTable = newTable.querySelector('.table-bordered tbody');
+        const newMaterialRow = document.createElement('tr');
+        newMaterialRow.innerHTML = `
+        <td>
+            <input type="text" class="form-control form-control-sm materialCode" placeholder="Código">
+            <!-- Lista de sugestões -->
+        
+        </td>
+        <td>
+            <input type="text" class="form-control form-control-sm materialName" placeholder="Nome" readonly>
+        </td>
+        <td>
+            <input type="number" class="form-control form-control-sm" placeholder="Quantidade">
+        </td>
+        <td class="text-center">
+            <button type="button" class="btn btn-outline-danger btn-sm rounded-circle removeMaterialBtn">
+                <i class="bi bi-trash"></i>
+            </button>
+        </td>
+    `;
+
+        // Lógica para buscar nome do material
+        const materialCodeInput = newMaterialRow.querySelector('.materialCode');
+        const materialNameInput = newMaterialRow.querySelector('.materialName');
+
+        // Evento de input para buscar por código ou nome
+        materialCodeInput.addEventListener('input', () => {
+            const searchTerm = materialCodeInput.value.trim().toLowerCase();  // Valor de busca (código ou nome)
+
+            // Validação dos campos
+            const material = listaDeMateriais.find(item =>
+                item.id.toString().includes(searchTerm) || item.nome.toLowerCase().includes(searchTerm)
+            );
+
             if (material) {
-                materialNameInput.value = material.nome;
-                input.classList.remove('is-invalid');
+                materialNameInput.value = material.nome; // Preenche o nome
+                materialCodeInput.classList.remove('is-invalid');
                 materialNameInput.classList.remove('is-invalid');
             } else {
                 materialNameInput.value = ''; // Limpa o nome se o material não for encontrado
-                input.classList.add('is-invalid');
+                materialCodeInput.classList.add('is-invalid');
                 materialNameInput.classList.add('is-invalid');
             }
         });
-    });
-
-    return newTable;
-}
 
 
-    // Função para adicionar material a um nível
-function adicionarMaterial(newTable) {
-    const materialTable = newTable.querySelector('.table-bordered tbody');
-    const newMaterialRow = document.createElement('tr');
-    newMaterialRow.innerHTML = `
-        <td><input type="text" class="form-control form-control-sm materialCode" placeholder="Código"></td>
-        <td><input type="text" class="form-control form-control-sm materialName" placeholder="Nome" readonly></td>
-        <td><input type="number" class="form-control form-control-sm" placeholder="Quantidade"></td>
-        <td class="text-center">
-            <button type="button" class="btn btn-danger btn-sm rounded-circle removeMaterialBtn"><i class="bi bi-trash"></i></button>
-        </td>
-    `;
-    
-    // Lógica para buscar nome do material
-    const materialCodeInput = newMaterialRow.querySelector('.materialCode');
-    const materialNameInput = newMaterialRow.querySelector('.materialName');
+        // Lógica para remover o material
+        const removeMaterialBtn = newMaterialRow.querySelector('.removeMaterialBtn');
+        removeMaterialBtn.addEventListener('click', () => newMaterialRow.remove());
 
-    materialCodeInput.addEventListener('input', () => {
-        const codigo = materialCodeInput.value.trim();
-        const material = listaDeMateriais.find(item => item.id.toString() === codigo);
-
-        if (material) {
-            materialNameInput.value = material.nome;
-            materialCodeInput.classList.remove('is-invalid');
-            materialNameInput.classList.remove('is-invalid');
-        } else {
-            materialNameInput.value = ''; // Limpa o nome se o material não for encontrado
-            materialCodeInput.classList.add('is-invalid');
-            materialNameInput.classList.add('is-invalid');
-        }
-    });
-
-    // Lógica para remover o material
-    const removeMaterialBtn = newMaterialRow.querySelector('.removeMaterialBtn');
-    removeMaterialBtn.addEventListener('click', () => newMaterialRow.remove());
-
-    materialTable.appendChild(newMaterialRow);
-}
+        materialTable.appendChild(newMaterialRow);
+    }
 
     // Carregar os níveis ao abrir o modal
     carregarNiveis(posteId);
@@ -212,13 +253,21 @@ function adicionarMaterial(newTable) {
 
     const saveBtn = document.getElementById(`saveBtn_${posteId}`);
     saveBtn.addEventListener('click', async () => {
+
+        // Alterar o texto do botão e desativá-lo
+        saveBtn.innerHTML = `
+ <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+ Salvando...
+`;
+        saveBtn.disabled = true;
+
         const tabelasContainer = document.getElementById(`tabelasContainer_${posteId}`);
         const niveis = [];
-        
+
         tabelasContainer.querySelectorAll('.card').forEach((card) => {
             const nivelId = card.getAttribute('data-nivel-id'); // Recupera o ID do nível
             const nivelNome = card.querySelector('input').value.trim(); // Recupera o nome do nível
-            
+
             const materiais = [];
             card.querySelectorAll('.table-bordered tbody tr').forEach((row) => {
                 const codigo = row.querySelector('td:nth-child(1) input').value;
@@ -236,10 +285,10 @@ function adicionarMaterial(newTable) {
                 });
             }
         });
-    
+
         console.log(`ID da Estrutura: ${posteId}`);
         console.log(niveis);
-    
+
         // Envia os dados para o servidor
         try {
             const response = await fetch('/salvar_niveis_materiais', {
@@ -252,17 +301,20 @@ function adicionarMaterial(newTable) {
                     niveis: niveis,
                 }),
             });
-    
+
             const result = await response.json();
-            
+
             if (response.ok) {
                 console.log('Níveis salvos ou atualizados com sucesso!', result);
                 showAlert('Níveis salvos ou atualizados com sucesso!', 'success');
-                
+
                 // Fecha o modal após sucesso
                 const modalInstance = bootstrap.Modal.getInstance(modal);
                 modalInstance.hide();  // Fecha o modal
-                
+
+                saveBtn.innerHTML = 'Salvar';
+                saveBtn.disabled = false;
+
             } else {
                 console.error('Erro ao salvar os níveis:', result.error);
                 showAlert('Erro ao salvar os níveis. Tente novamente.', 'danger');
@@ -272,7 +324,7 @@ function adicionarMaterial(newTable) {
             showAlert('Erro ao enviar os dados para o servidor. Tente novamente.', 'danger');
         }
     });
-    
-    
-    
+
+
+
 }
