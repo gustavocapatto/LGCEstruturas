ea402d045e6af61b6ebc26db8c6cda42c1512b28
Luiz Gustavo Capatto <120795937+gustavocapatto@users.noreply.github.com>
2024-12-02 09:48:39 -0300

    Update dbConfig.js

--- functions/dbConfig.js
+++ functions/dbConfig.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2/promise');
 
 const dbConfig = {
-    host: 'sql.freedb.tech',
-    user: 'freedb_lgcestrutura',
-    password: 'ruVgU3t&3A?ArP7',
-    database: 'freedb_lgcestrutura',
+    host: 'bijtknu4l3rjuqugby03-mysql.services.clever-cloud.com',
+    user: 'bijtknu4l3rjuqugby03',
+    password: 'gNEEzajDVzD52FDocn0B',
+    database: 'bijtknu4l3rjuqugby03',
 };
 
 const getConnection = async () => {
